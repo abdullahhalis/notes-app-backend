572b58a3a90b5cbcfd8d7a6c6947d173ac06dacc
Abdullah Hali Saputra <abdullahhali060@gmail.com>
2023-05-07 16:02:30 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addNoteHandler,
+  getAllnotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteById,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllnotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteById,
+  },
+];
+
+module.exports = routes;
